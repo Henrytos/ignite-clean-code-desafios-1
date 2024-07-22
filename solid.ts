50b5b0f49ec1dc40687d26b44bb94e426d9fd190
Henry <franzhenry46@gmail.com>
2024-07-22 16:50:23 -0300

    respetando single of responsibility principle

--- solid.ts
+++ solid.ts
@@ -6,6 +6,45 @@
     D => dependency inversion responsibility  principle  
 */
 
-class getOrderDiscount {
-  execute() {}
+interface ProductProps {
+  name: string;
+  price: number;
 }
+class Product {
+  private props: ProductProps;
+
+  constructor(name: string, price: number) {
+    this.props = { name, price };
+  }
+
+  get name() {
+    return this.name;
+  }
+  get price() {
+    return this.price;
+  }
+}
+
+class GetOrderDiscount {
+  execute(amount: number, methodPayment: string) {
+    if (methodPayment == "billet") {
+      return amount * 0.1;
+    }
+
+    if (methodPayment == "credit") {
+      return amount * 0;
+    }
+
+    if (methodPayment == "debit") {
+      return amount * 0.05;
+    }
+
+    return 0;
+  }
+}
+
+const order = new Product("camisa", 1000);
+
+const getOrderDiscount = new GetOrderDiscount();
+
+getOrderDiscount.execute(order.price, "billet");
